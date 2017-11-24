e6e22de317dbda9bd350d47124070427245ba3fe
victoRLin6 <33682892+victoRLin6@users.noreply.github.com>
2017-11-24 10:47:59 +0100

    Add files via upload

---
+++ Problema03P6.py
@@ -0,0 +1,10 @@
+print("casilla torre")
+x=int(input("x="))
+y=int(input("y="))
+print("casilla para realizar movimiento")
+x1=int(input("x="))
+y1=int(input("y="))
+if(x==x1)or(y==y1):
+  print("SI")
+else:
+  print("NO")
