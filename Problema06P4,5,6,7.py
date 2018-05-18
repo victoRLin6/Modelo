544ec9da02db07f0bf453afd758c77c52d93c5b1
victoRLin6 <33682892+victoRLin6@users.noreply.github.com>
2018-05-18 10:58:47 +0200

    Add files via upload

---
+++ Problema06P4,5,6,7.py
@@ -0,0 +1,24 @@
+m=1
+x=0
+y=0
+g=0
+while True:
+  
+  n=int(input('Da un número:'))
+  
+  if n < g:
+    g = g
+  else:
+    g = n
+  if n==0:
+   
+    break
+  
+  x+=1
+  y=y+n
+print('Cantidad de numeros:'+str(x))
+print('Suma numeros:'+str(y))
+
+m=y/(x)
+print('Media:'+ str(m))
+print('Mas grande:'+ str(g))
